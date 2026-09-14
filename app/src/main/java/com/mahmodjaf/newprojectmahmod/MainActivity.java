2f268138d16c2fded81a34a53643897f5efceaab
class01 <hamoniral@gmail.com>
2026-09-14 09:20:23 +0300

    ;l.;'l;'l

--- app/src/main/java/com/mahmodjaf/newprojectmahmod/MainActivity.java
+++ app/src/main/java/com/mahmodjaf/newprojectmahmod/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //5656+56+5
+        //5656+56+5jkghkghbjkghj
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
